b0f5b4c1db1b80c257abe90b481d334ef63bf23a
やっこ@dotlaboratory <93868999+yakkoworks@users.noreply.github.com>
2025-08-20 01:01:24 +0900

    Add files via upload

---
+++ 右クリック禁止を禁止.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         右クリック禁止を禁止
+// @namespace    http://tampermonkey.net/
+// @version      2023-12-23
+// @description  try to take over the world!
+// @author       You
+// @match        https://app.nizikano-2d.jp/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.addEventListener('contextmenu', function(e) {
+  e.preventDefault(); // デフォルトのコンテキストメニューを抑制
+}, false);
+})();
